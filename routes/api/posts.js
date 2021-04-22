ed6fca3a71df73ba9fdd90dccd9aa65420b66501
Bzch <francobaisch00@gmail.com>
2021-04-22 00:38:49 -0300

    added error handling inside requests

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,24 +5,36 @@ const { Post } = require("../../db");
 
 //GET /posts/
 router.get("/", async (req, res) => {
-  const posts = await Post.findAll();
-  res.json(posts);
+  try {
+    const posts = await Post.findAll();
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //GET /posts/:id
 
 //POST /posts/
 router.post("/", async (req, res) => {
-  const post = await Post.create(req.body);
-  res.json(post);
+  try {
+    const post = await Post.create(req.body);
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //PATCH /posts/:id
 router.patch("/:postId", async (req, res) => {
-  await Post.update(req.body, {
-    where: { id: req.params.postId },
-  });
-  res.json({ success: "Post updated!" });
+  try {
+    await Post.update(req.body, {
+      where: { id: req.params.postId },
+    });
+    res.json({ success: "Post updated!" });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //DELETE /posts/:id
